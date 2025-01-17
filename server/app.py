7f245ff73f653681f872be0a020e894dd5137260
Stephen Waithumbi <stephenwaithumbi14@gmail.com>
2025-01-17 12:43:56 +0300

    flask lab

--- server/app.py
+++ server/app.py
@@ -3,3 +3,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h2>Welcome to my page! </h2>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h2>Profile for {username}</h2>'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
